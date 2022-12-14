3f789e8cc31f4c6efea596deb42c6202900bfb4b
Andrija Antic <andrijant0304@gmail.com>
2022-12-14 08:58:39 +0100

    Vezbanje1 'Hangman'

--- src/main/java/p13_12_2022/Vezbanje1.java
+++ src/main/java/p13_12_2022/Vezbanje1.java
@@ -4,6 +4,16 @@ public class Vezbanje1 {
 
     public static void main(String[] args) {
 
+        String name = "H a n g m a n";
+
+        System.out.println(name);
+        System.out.println("- - - -");
+        System.out.println("|      |");
+        System.out.println("|      o");
+        System.out.println("|     /|\\");
+        System.out.println("|      |");
+        System.out.println("|");
+        System.out.println("- - - -  - - - A -");
 
     }
 }
