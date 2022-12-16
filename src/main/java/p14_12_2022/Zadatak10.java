665a2bf8debec5c2ba0575d85e0a9cb9873a022e
Andrija Antic <andrijant0304@gmail.com>
2022-12-17 00:25:26 +0100

    p14_12_2022/Zadatak10 dopuna

--- src/main/java/p14_12_2022/Zadatak10.java
+++ src/main/java/p14_12_2022/Zadatak10.java
@@ -11,14 +11,14 @@ public class Zadatak10 {
         System.out.print("Unesi broj A: ");
         int a = s.nextInt();
 
-        if ( a%2 == 0){
+        if (( a%2 == 0) && ( a%3 == 0)){
+            System.out.println("Broj A je deljiv i sa brojem 2 i sa brojem 3 ");
+        } else if ( a%2 == 0){
             System.out.println("Broj A je deljiv sa brojem 2");
         } else if ( a%3 == 0) {
             System.out.println("Broj A je deljiv sa brojem 3");
         } else {
-            System.out.println("Broj A nije deljiv ni sa brojm 2 ni sa brojem 3");
+            System.out.println("Broj A nije deljiv ni sa brojem 2 ni sa brojem 3");
         }
-
     }
-
 }
