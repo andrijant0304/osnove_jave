929361bf0dea966513c92c1be23b1e1c7ead24df
Andrija Antic <andrijant0304@gmail.com>
2022-12-20 18:53:39 +0100

    WHILE loop 20.12.2022

---
+++ src/main/java/p_20_12_2022_while_loop.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class p_20_12_2022_while_loop {
+
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        int number = 0;
+
+        while (number != 0) {
+            System.out.println("Unesi broj: ");
+            number = s.nextInt();
+
+            System.out.println("Unet broj je " + number);
+        }
+        System.out.println("Unet broj je nula. END");
+    }
+}
