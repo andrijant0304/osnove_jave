cea07b57f6a8b9a90af7ec98150b3b6321ab3c71
Andrija Antic <andrijant0304@gmail.com>
2023-01-14 09:02:42 +0100

    Priprema za prvi Java test

---
+++ src/main/java/Test/Zadatak2.java
@@ -0,0 +1,17 @@
+package Test;
+
+public class Zadatak2 {
+}
+
+
+//    Napisati program koji učitava brojeve od korisnika dok ne unese dve nule
+//    zaredom.Na kraju programa ispisati sumu unetih brojeva.
+//        Primer izvršenja 1:
+//        Unesite broj: 1
+//        Unesite broj: 1
+//        Unesite broj: 3
+//        Unesite broj: 5
+//        Unesite broj: 9
+//        Unesite broj: 0
+//        Unesite broj: 0
+//        Suma je 19
---
+++ src/main/java/Test/ZadatakTest.java
@@ -0,0 +1,19 @@
+package Test;
+
+public class ZadatakTest {
+
+
+    public static void main(String[] args) {
+        int x = suma(10, 5);
+
+        System.out.println(x);
+
+    }
+
+    public static int suma (int a, int b) {
+        int suma = a + b;
+
+        return suma + 15;
+    }
+
+}
